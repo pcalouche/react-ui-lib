22b69ef263c3697277b3b81d4f4c74c3797e2a43
Philip Calouche <philip.calouche@outlook.com>
2020-07-09 12:09:20 -0400

    initial commit of typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
-import babel from 'rollup-plugin-babel';
+// import babel from 'rollup-plugin-babel';
+import typescript from 'rollup-plugin-typescript2';
+// import typescript from '@rollup/plugin-typescript';
 import image from 'rollup-plugin-img';
 import nodeResolve from '@rollup/plugin-commonjs';
 import commonjs from '@rollup/plugin-node-resolve';
@@ -7,49 +9,49 @@ import {terser} from 'rollup-plugin-terser';
 import packageJson from './package.json';
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   external: [
     ...Object.keys(packageJson.devDependencies),
     ...Object.keys(packageJson.peerDependencies)
   ],
-  output: [{
-    file: packageJson.main,
-    format: 'cjs',
-    sourcemap: true
-  }, {
-    file: packageJson.module,
-    format: 'es',
-    sourcemap: true
-  }],
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: packageJson.module,
+      format: 'es',
+      sourcemap: true
+    }
+  ],
   watch: {
     clearScreen: false
   },
   plugins: [
-    babel({
-      presets: [
-        [
-          '@babel/env',
-          {
-            'loose': true,
-            'shippedProposals': true,
-            'modules': false
-          }
-        ],
-        '@babel/react'
-      ],
-      plugins: [
-        '@babel/plugin-proposal-object-rest-spread',
-        '@babel/plugin-proposal-class-properties'
-      ],
-      exclude: 'node_modules/**'
-    }),
+    // babel({
+    //   presets: [
+    //     [
+    //       '@babel/env',
+    //       {
+    //         'shippedProposals': true,
+    //         'modules': false
+    //       }
+    //     ],
+    //     '@babel/react'
+    //   ],
+    //   plugins: [
+    //     '@babel/plugin-proposal-object-rest-spread',
+    //     '@babel/plugin-proposal-class-properties'
+    //   ],
+    //   exclude: 'node_modules/**'
+    // }),
+    typescript(),
     nodeResolve(),
     commonjs(),
     image({
       limit: 1024 * 100 //100K max
-    }),
-    babel({
-      exclude: 'node_modules/**'
     }),
     sizeSnapshot(),
     terser()
