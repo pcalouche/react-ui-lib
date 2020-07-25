194f29d19d789409c3930ce7635766ab85d1c2d4
Philip Calouche <philip.calouche@tma1.com>
2020-07-25 00:25:01 -0400

    config and readme updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
+// import babel from '@rollup/plugin-babel';
 import typescript from 'rollup-plugin-typescript2';
 import image from '@rollup/plugin-image';
 import {sizeSnapshot} from 'rollup-plugin-size-snapshot';
@@ -13,25 +13,27 @@ export default [
       ...Object.keys(packageJson.devDependencies)
     ],
     output: [
-      {file: packageJson.main, format: 'cjs'},
-      {file: packageJson.module, format: 'es'}
+      {file: packageJson.main, format: 'cjs', sourcemap: true},
+      {file: packageJson.module, format: 'es', sourcemap: true}
     ],
     plugins: [
       nodeResolve(),
-      babel({
-        babelHelpers: 'runtime',
-        babelrc: false,
-        inputSourceMap: true,
-        sourceMaps: 'both',
-        presets: [
-          ['@babel/env', {'modules': false}],
-          ['@babel/react']
-        ],
-        plugins: [
-          ['@babel/plugin-transform-runtime', {'useESModules': true}]
-        ],
-        exclude: ['node_modules/**']
-      }),
+      // Remove babel plugin and uninstall @rollup/plugin-babel if your library is 100% TypeScript
+      // babel({
+      //   babelHelpers: 'runtime',
+      //   babelrc: false,
+      //   inputSourceMap: true,
+      //   sourceMaps: 'both',
+      //   presets: [
+      //     ['@babel/env', {'modules': false}],
+      //     ['@babel/react']
+      //   ],
+      //   plugins: [
+      //     ['@babel/plugin-transform-runtime', {'useESModules': true}]
+      //   ],
+      //   exclude: ['node_modules/**']
+      // }),
+      // Remove typescript plugin and uninstall rollup-plugin-typescript2 if your library is 100% JavaScript
       typescript(),
       image(),
       sizeSnapshot()
